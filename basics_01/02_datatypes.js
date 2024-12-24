f884f79431d73b1d114f59d800e1bd4923c516b0
AnveshDhakad <149469454+AnveshDhakad@users.noreply.github.com>
2024-12-24 08:54:30 +0000

    datatypes

---
+++ basics_01/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // this statement treat all js code in newer version.
+
+//alert( 3 + 3 ) //we are using node.js not browser so this will not run simply as in browser.
+
+console.log( 3 
+    +3 ) //code readability should be high 
+    
+    let name = "Shyam"
+    let age = 21 
+    let isLoggedIn = false
+    let state;
+     
+   /* primitive data types
+   Number
+   Null //stand alone value
+   String
+   Symbol   //mainly used in react to find unique components
+   Undefined
+   Bollean // true/false
+   BigInt //used to large values mainly in stockmarket or bank etc.
+   */
+
+   //Non primitive data types 
+   //object
+
+   console.log(typeof isLoggedIn)
