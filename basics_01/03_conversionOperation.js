4a6c7d64f0833758e07c9f043e699224ed5df1d9
AnveshDhakad <149469454+AnveshDhakad@users.noreply.github.com>
2025-01-06 04:23:10 +0000

    conversion of data types..

---
+++ basics_01/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33abs"
+//console.log(typeof score);
+
+let valueInNumber =Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+//conversions 
+// "33" => 33
+// "33abc" => Nan
+// true => 1 false =>0
+
+let isLoggedIn = "Anvesh"
+let bolleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(bolleanIsLoggedIn)
+
+// 1 => True 0 => False
+// "" => false  
+//"Anvesh" => True
+
+let someNumber = 33
+let stringnumber = String(someNumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
