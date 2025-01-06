81d480d900ebe5cc52e47c5fdf8e471b2334b545
AnveshDhakad <149469454+AnveshDhakad@users.noreply.github.com>
2025-01-06 05:42:29 +0000

    prefixAndPostfix

--- basics_01/03_conversionOperation.js
+++ basics_01/03_conversionOperation.js
@@ -20,5 +20,40 @@ let bolleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringnumber = String(someNumber)
-console.log(stringnumber)
-console.log(typeof stringnumber)
+//console.log(stringnumber)
+//console.log(typeof stringnumber)
+
+//--------------*Operations*-----------------*
+
+let value = 3
+let negValue = - value
+//console.log(negValue)
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "Anvesh"
+let str2 = " Dhakad"
+let str3 = str1 + str2
+//console.log(str3)
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+console.log( 3 + 4 * 5 % 3); //not good practise
+console.log(+true) //not good practise
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 //not good practise
+
+let gamecounter = 50 
+// ++gamecounter ; //prefix
+gamecounter++ ; //postfix
+console.log(gamecounter)
+
